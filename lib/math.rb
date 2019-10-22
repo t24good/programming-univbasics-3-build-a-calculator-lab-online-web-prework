6a2b0a32d5451f5bdb4fc0335f50b4174ed31244
Tanesha Goodman <t24good@icloud.com>
2019-10-22 03:07:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
-def addition(num1, num2)
-
+def addition(num1 = 4, num2 = 5)
+  puts (num1 + num2)
+  (num1 + num2)
 end
 
-def subtraction(num1, num2)
-
+def subtraction(num1 = 10, num2 = 5)
+  puts (num1 - num2)
+  (num1 - num2)
 end
 
-def division(num1, num2)
-
+def division(num1 = 50, num2 = 2)
+  puts (num1 / num2)
+  (num1 / num2)
 end
 
-def multiplication(num1, num2)
-
+def multiplication(num1 = 4, num2 =30)
+  puts (num1 * num2)
+  (num1 * num2)
 end
 
 def modulo(num1, num2)
